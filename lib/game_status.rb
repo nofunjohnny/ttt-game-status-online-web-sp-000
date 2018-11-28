f148603b8307932a9181a029295e5181b7b7fb0d
John Zapata <john.zapata@thezapata.com>
2018-11-28 01:02:18 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,7 +17,7 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   if board.all? {|current| current.nil? || current == ' '}
-    return true
+    return false
   else
     WIN_COMBINATIONS.each do|combination|
       character = board[combination[0]]
